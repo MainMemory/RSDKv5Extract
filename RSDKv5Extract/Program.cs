75cbd5029fdf30af0abd6a983130a0a8089385b2
MainMemory <sonicmike2@yahoo.com>
2022-06-28 23:25:06 -0500

    Add project files.

---
+++ RSDKv5Extract/Program.cs
@@ -0,0 +1,37 @@
+﻿using RSDKv5;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RSDKv5Extract
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("\tRSDKv5Extract Data.rsdk [FileList.txt] [OutputFolder]");
+				return;
+			}
+			System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+			stopwatch.Start();
+			string filelist = args.Length > 1 ? args[1] : Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "rsdk_files_list.txt");
+			string outpath = args.Length > 2 ? args[2] : Path.ChangeExtension(args[0], null);
+			List<string> filenames = null;
+			if (File.Exists(filelist))
+				filenames = new List<string>(File.ReadAllLines(filelist));
+			Console.WriteLine("Extracting files...");
+			DataPack dataPack = new DataPack(args[0], filenames);
+			foreach (var file in dataPack.files)
+			{
+				string path = Path.Combine(outpath, file.name.name);
+				Directory.CreateDirectory(Path.GetDirectoryName(path));
+				File.WriteAllBytes(path, file.data);
+			}
+			File.WriteAllText(Path.Combine(outpath, "RSDKVer.txt"), ((char)dataPack.version).ToString());
+			stopwatch.Stop();
+			Console.WriteLine("Extracted {0} files in {1} seconds.", dataPack.files.Count, stopwatch.ElapsedMilliseconds / 1000m);
+		}
+	}
+}
---
+++ RSDKv5Pack/Program.cs
@@ -0,0 +1,33 @@
+﻿using RSDKv5;
+using System;
+using System.IO;
+
+namespace RSDKv5Pack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("\tRSDKv5Pack DataFolder [Data.rsdk]");
+				return;
+			}
+			System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+			stopwatch.Start();
+			DataPack dataPack = new DataPack();
+			string rsdkverpath = Path.Combine(args[0], "RSDKVer.txt");
+			if (File.Exists(rsdkverpath))
+				dataPack.version = (byte)File.ReadAllText(rsdkverpath)[0];
+			foreach (var file in Directory.EnumerateFiles(args[0], "*", SearchOption.AllDirectories))
+				dataPack.files.Add(new DataPack.File()
+				{
+					name = new NameIdentifier(file.Substring(args[0].Length + 1)),
+					data = File.ReadAllBytes(file)
+				});
+			dataPack.Write(args.Length > 1 ? args[1] : args[0] + ".rsdk");
+			stopwatch.Stop();
+			Console.WriteLine("Wrote {0} files in {1} seconds.", dataPack.files.Count, stopwatch.ElapsedMilliseconds / 1000m);
+		}
+	}
+}
